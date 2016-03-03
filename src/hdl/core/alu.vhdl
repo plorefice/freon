cf1e4d65488c5f4ae3044a7789073d05d1fb8f94
Pietro Lorefice <pietro.lorefice@gmail.com>
2016-03-03 08:21:49 +0100

    [core] alu: simplified architecture

--- src/hdl/core/alu.vhdl
+++ src/hdl/core/alu.vhdl
@@ -17,66 +17,48 @@ entity alu is
 end entity ; -- alu
 
 architecture beh of alu is
-	signal op1_u, op2_u, res_u : unsigned(XLEN-1 downto 0) := (others => '0');
-	signal op1_s, op2_s, res_s : signed(XLEN-1 downto 0) := (others => '0');
-	signal res_v : std_logic_vector(XLEN-1 downto 0) := (others => '0');
+	signal op1_s, op2_s : signed(XLEN-1 downto 0) := (others => '0');
+	signal op1_u, op2_u : unsigned(XLEN-1 downto 0) := (others => '0');
 begin
-	-- Connecting inputs
-	op1_u <= unsigned(op1);
-	op2_u <= unsigned(op2);
-
+	-- Connecting internal signals
 	op1_s <= signed(op1);
 	op2_s <= signed(op2);
 
-	-- Connecting outputs
-	res <= res_v                 when opsel(2) = '1' else	-- Logic operation
-	     std_logic_vector(res_u) when opsel(0) = '1' else	-- SLTU
-	     std_logic_vector(res_s);				-- All others
+	op1_u <= unsigned(op1);
+	op2_u <= unsigned(op2);
 
 	-- ALU arithmetic operators
-	process (op1_s, op2_s, op1_u, op2_u, opsel, ctrl)
+	process (op1_s, op2_s, op1_u, op2_u, op1, op2, opsel, ctrl)
 	begin
-		res_u <= (others => '0');
-		res_s <= (others => '0');
-
-		case opsel(1 downto 0) is
-			when "00" => -- ADD/SUB
+		case opsel(2 downto 0) is
+			when "000" => -- ADD/SUB
 				if (ctrl = '0') then
-					res_s <= op1_s + op2_s;
+					res <= std_logic_vector(op1_s + op2_s);
 				else
-					res_s <= op1_s - op2_s;
+					res <= std_logic_vector(op1_s - op2_s);
 				end if;
 
-			when "10" => -- SLT
+			when "010" => -- SLT
 				if (op1_s < op2_s) then
-					res_s(0) <= '1';
+					res(0) <= '1';
 				end if;
 
-			when "11" => -- SLTU
+			when "011" => -- SLTU
 				if (op1_u < op2_u) then
-					res_u(0) <= '1';
+					res(0) <= '1';
 				end if;
 
+			when "100" => -- XOR
+				res <= op1 xor op2;
+
+			when "110" => -- OR
+				res <= op1 or op2;
+
+			when "111" => -- AND
+				res <= op1 and op2;
+
 			when others => -- irrelevant, not managed by ALU
-				res_u <= (others => '0');
-				res_v <= (others => '0');
-
-		end case;
-	end process;
-
-	-- ALU logic operators
-	process (op1, op2, opsel, ctrl)
-	begin
-		case opsel(1 downto 0) is
-			when "00" => -- XOR
-				res_v <= op1 xor op2;
-			when "10" => -- OR
-				res_v <= op1 or op2;
-			when "11" => -- AND
-				res_v <= op1 and op2;
-			when others => -- irrelevant, not managed by ALU
-				res_v <= (others => '0');
-
+				res <= (others => '0');
 		end case;
 	end process;
 
